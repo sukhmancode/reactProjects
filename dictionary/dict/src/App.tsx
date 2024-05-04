c36f596dc90feb52c27692f9e1fdc92f3146fd38
sukhmancode <coder4614@gmail.com>
2024-05-05 00:10:40 +0530

    proj 1 added

---
+++ dictionary/dict/src/App.tsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="nav-bar">
+          <div className="logo">
+            <img src="logo.svg" alt="" />
+          </div>
+          <div className="top-icons">
+            <div className="language-dropdown">
+              <h1>Serif</h1>
+            </div>
+             
+             <div className="toggle-btn">
+              <div className="toggle-circle">
+                
+              </div>
+             </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
